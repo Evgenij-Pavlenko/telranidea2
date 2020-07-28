a4c503db476b0c16f1dd541280dd64cf6c560ca7
Evgenij Pavlenko <evgenij.pavlenko@gmail.com>
2020-07-29 00:49:05 +0200

    Task1 - boolean isDevided()

---
+++ 20200728_HW/src/SortedListMultTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortedListMultTest {
+    SortedListMult mult = new SortedListMult();
+
+    @Test
+    public void testSortedList_nIs33_true(){
+        List<Integer> toTest = Arrays.asList(1,3,8,9,9,11,20);
+        assertTrue(mult.isDevided(toTest, 33));
+    }
+
+    @Test
+    public void testSortedList_nIs64_false(){
+        List<Integer> toTest = Arrays.asList(1,3,8,9,9,11,20);
+        assertFalse(mult.isDevided(toTest, 64));
+    }
+
+    @Test
+    public void testSortedList_nIs81_true(){
+        List<Integer> toTest = Arrays.asList(1,3,8,9,9,11,20);
+        assertTrue(mult.isDevided(toTest, 81));
+    }
+
+
+}
