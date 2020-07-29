2b4a3ec900d3b27868462b63d71cae134d5d2b6c
Evgenij Pavlenko <evgenij.pavlenko@gmail.com>
2020-07-29 19:28:12 +0200

    Task3 - number power - add more test

--- 20200728_HW/src/NumberPowTest.java
+++ 20200728_HW/src/NumberPowTest.java
@@ -11,12 +11,22 @@ class NumberPowTest {
     }
 
     @Test
-    public void testNumberPow2_numAndPower_numInPower(){
+    public void testNumberPow2_num8AndPower2_numInPower64(){
         assertEquals(64, numberPow.numberToPow(8,2));
     }
 
     @Test
-    public void testNumberPow2_someNumAndPower0_numInPower(){
+    public void testNumberPow2_num3AndPower3_numInPower27(){
+        assertEquals(27, numberPow.numberToPow(3,3));
+    }
+
+    @Test
+    public void testNumberPow2_num2AndPower10_numInPower1024(){
+        assertEquals(1024, numberPow.numberToPow(2,10));
+    }
+
+    @Test
+    public void testNumberPow2_someNumAndPower0_numInPower1(){
         assertEquals(1, numberPow.numberToPow(8,0));
     }
 
