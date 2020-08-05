2397061e2108d656db34a66aff84fae81885cea1
Evgenij Pavlenko <evgenij.pavlenko@gmail.com>
2020-08-05 22:23:23 +0200

    HW20200304_Text with N size

---
+++ HomeWork/src/HW20200304_isLovely/TextWithN.java
@@ -0,0 +1,26 @@
+package HW20200304_isLovely;
+
+import java.util.Arrays;
+
+public class TextWithN {
+
+    public String makeNewText (String s, int N){
+        if (N>=s.length()){
+            return s;
+        }
+        for (int i = N; i >=0 ; i--) {
+            if (s.charAt(i)==' '){
+            return s.substring(0, i);
+            }
+        }
+return "";
+
+//        s.chars()
+//                .limit(N)
+//                .
+//        String[] strArr = s.split(" ");
+//       return Arrays.stream(strArr)
+//                .filter(str->str.length()<=N).findFirst().orElse("");
+
+    }
+}
---
+++ HomeWork/src/HW20200304_isLovely/TextWithNTest.java
@@ -0,0 +1,37 @@
+package HW20200304_isLovely;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextWithNTest {
+    TextWithN text = new TextWithN();
+
+    @Test
+    public void testMakeText_HelloWorldAndN11_HelloWorld(){
+        String s = "Hello world";
+        assertEquals("Hello world", text.makeNewText(s, 11));
+    }
+    @Test
+    public void testMakeText_HelloWorldAndN5_Hello(){
+        String s = "Hello world";
+        assertEquals("Hello", text.makeNewText(s, 5));
+    }
+    @Test
+    public void testMakeText_HelloWorldAndN7_Hello(){
+        String s = "Hello world";
+        assertEquals("Hello", text.makeNewText(s, 7));
+    }
+    @Test
+    public void testMakeText_HelloWorldAndN4_emptyString(){
+        String s = "Hello world";
+        assertEquals("", text.makeNewText(s, 4));
+    }
+    @Test
+    public void testMakeText_HelloWorlAndYoudAndN17_emptyString(){
+        String s = "Hello world and you!";
+        assertEquals("Hello world and", text.makeNewText(s, 17));
+    }
+
+
+}
