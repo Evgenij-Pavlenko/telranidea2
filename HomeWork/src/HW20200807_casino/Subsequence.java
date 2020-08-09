f29988d151eefd7c320c1af299bd0dcfd4fdc309
Evgenij Pavlenko <evgenij.pavlenko@gmail.com>
2020-08-09 20:19:38 +0200

    HW20200807-Subsequence with only String

--- HomeWork/src/HW20200807_casino/Subsequence.java
+++ HomeWork/src/HW20200807_casino/Subsequence.java
@@ -16,17 +16,33 @@ public class Subsequence {
      * @return is subsequence in sequence
      */
     public boolean isSubsequence(String first, String second) {
-        char[] chFirst = first.toCharArray();
-        char[] chSecond = second.toCharArray();
-        int count = 0;
+        // variant 1 with for
+//        char[] chFirst = first.toCharArray();
+//        char[] chSecond = second.toCharArray();
+//        int count = 0;
+//
+//        for (int i = 0, j = 0; j < chSecond.length && i < chFirst.length; j++) {
+//            if (chFirst[i] == chSecond[j]) {
+//                count++;
+//                i++;
+//            }
+//        }
+//        if (count == chFirst.length) {
+//            return true;
+//        }
 
-        for (int i = 0, j = 0; j < chSecond.length && i < chFirst.length; j++) {
-            if (chFirst[i] == chSecond[j]) {
+        //variant 2 with String - is this faster?
+        char[] chFirst = first.toCharArray();
+        int intSequence = -1; // if at place 0
+        int count = 0;
+        for (char ch : chFirst) {
+            int temp = second.indexOf(ch);
+            if (temp > intSequence) {
+                intSequence = temp;
                 count++;
-                i++;
             }
         }
-        if (count == chFirst.length) {
+        if (count == first.length()) {
             return true;
         }
         return false;
