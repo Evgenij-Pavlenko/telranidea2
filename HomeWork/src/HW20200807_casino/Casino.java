68587d02d054ea37c9c705be64bf71f32d01d753
Evgenij Pavlenko <evgenij.pavlenko@gmail.com>
2020-08-11 15:11:57 +0200

    HW20200810-CasinoAndVasja - with recursion - all right

--- HomeWork/src/HW20200807_casino/Casino.java
+++ HomeWork/src/HW20200807_casino/Casino.java
@@ -16,9 +16,7 @@ public class Casino {
      */
     public int minCountGame(int N, int M) {
 //        //обнуляем 1 доллар)
-//        if (N == 0) { // но кастыль( позже попробую через рекурсию
-//            return 1;
-//        }
+//
 //        int count = 0;
 //        while (N > 1) {
 //            if (N % 2 == 0 && M > 0) {
@@ -30,6 +28,9 @@ public class Casino {
 //                count++;
 //            }
 //        }
+        if (N == 0) { // все равно костыль( - или это норм?
+            return 1;
+        }
         return loopGame(N, M);
 
     }
