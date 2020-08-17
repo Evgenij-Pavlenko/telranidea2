e84c45a143c1f90d907dcee0f27f25e40befc9a8
Evgenij Pavlenko <evgenij.pavlenko@gmail.com>
2020-08-18 01:01:53 +0200

    HW20200817 - Hunger_Games-Tarakan-Edition

---
+++ HomeWork/src/HungerGames.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class HungerGames {
+    public static void main(String[] args) throws InterruptedException {
+        List<Score> score = new ArrayList<>();
+        Tarakan t1 = new Tarakan("t1", 10, score);
+        Tarakan t2 = new Tarakan("t2", 10, score);
+        Tarakan t3 = new Tarakan("t3", 10, score);
+        Tarakan t4 = new Tarakan("t4", 10, score);
+        Tarakan t5 = new Tarakan("t5", 10, score);
+        Tarakan t6 = new Tarakan("t6", 10, score);
+        Tarakan t7 = new Tarakan("t7", 10, score);
+        Tarakan t8 = new Tarakan("t8", 10, score);
+        Tarakan t9 = new Tarakan("t9", 10, score);
+        Tarakan t1000 = new Tarakan("t1000", 10, score);
+        Thread th1 = new Thread(t1);
+        Thread th2 = new Thread(t2);
+        Thread th3 = new Thread(t3);
+        Thread th4 = new Thread(t4);
+        Thread th5 = new Thread(t5);
+        Thread th6 = new Thread(t6);
+        Thread th7 = new Thread(t7);
+        Thread th8 = new Thread(t8);
+        Thread th9 = new Thread(t9);
+        Thread th1000 = new Thread(t1000);
+        th1.start();
+        th2.start();
+        th3.start();
+        th4.start();
+        th5.start();
+        th6.start();
+        th7.start();
+        th8.start();
+        th9.start();
+        th1000.start();
+        th1.join();
+        th2.join();
+        th3.join();
+        th4.join();
+        th5.join();
+        th6.join();
+        th7.join();
+        th8.join();
+        th9.join();
+        th1000.join();
+        score.sort(Comparator.comparingInt(Score::getScore));
+        // бывает: Exception in thread "main" java.lang.NullPointerException((
+        for (int i = 0; i < score.size(); i++) {
+            System.out.println("Place: " + (i+1) + ", "
+            + score.get(i));
+        }
+
+    }
+}
