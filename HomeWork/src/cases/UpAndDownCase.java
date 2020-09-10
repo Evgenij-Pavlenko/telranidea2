b8a3223b4b9b568f06311e25ab01cc5bdf23f7fe
Evgenij Pavlenko <evgenij.pavlenko@gmail.com>
2020-09-10 20:05:02 +0200

    Cases

---
+++ HomeWork/src/cases/UpAndDownCase.java
@@ -0,0 +1,18 @@
+//package cases;
+//
+//public class UpAndDownCase {
+//    public String changeCase(String s) {
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < s.length(); i++) {
+////            String temp = sb.substring(i, i+1);
+//            char ch = sb.charAt(i);
+//
+//            if (ch < 100) {
+//                sb.append(sb.substring(i, i + 1).toUpperCase());
+//            }else{
+//                sb.append(sb.substring(i, i + 1).toLowerCase());
+//
+//            }
+//        }
+//    }
+//}
