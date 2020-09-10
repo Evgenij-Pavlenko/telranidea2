27088f079cad90936305673e32c9a5e3fff13879
Evgenij Pavlenko <evgenij.pavlenko@gmail.com>
2020-09-10 22:14:40 +0200

    HW20200909- String Symbol with Stack

--- HomeWork/src/stringSymbol/StringSymbol.java
+++ HomeWork/src/stringSymbol/StringSymbol.java
@@ -9,22 +9,18 @@ import java.util.*;
 public class StringSymbol {
 
     public boolean isCorrect(String str) {
-//        Map<Character, Character> map = new HashMap<>();
-//        map.put('[', ']');
-//        map.put('{', '}');
-//        map.put('(', ')');
-        Deque q = new ArrayDeque();
+        Stack q = new Stack();
         char[] ch = str.toCharArray();
         for (int i = 0; i < ch.length; i++) {
             char thC = ch[i];
             if (thC == '[' || thC == '{' || thC == '(') {
                 q.add(thC);
             } else {
-                char fromQ = (char) q.getLast();
+                char fromQ = (char) q.peek();
                 if (fromQ == '[' && thC == ']' ||
                         fromQ == '{' && thC == '}' ||
                         fromQ == '(' && thC == ')') {
-                    q.removeLast();
+                    q.pop();
                 }
             }
         }
